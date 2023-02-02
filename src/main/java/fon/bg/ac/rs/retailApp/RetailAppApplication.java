ff1d72eb3fc594630899cf74ceab9da78fe28180
ZeljanaGrujic <zg20180045@gmail.com>
2023-02-02 19:41:39 +0100

    Initial commit

---
+++ src/main/java/fon/bg/ac/rs/retailApp/RetailAppApplication.java
@@ -0,0 +1,13 @@
+package fon.bg.ac.rs.retailApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailAppApplication.class, args);
+	}
+
+}
