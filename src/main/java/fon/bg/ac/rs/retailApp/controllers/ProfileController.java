79fd1bd57be4a8c0e207370ae0a3ba3163c0ca06
ZeljanaGrujic <zg20180045@gmail.com>
2023-02-12 16:20:47 +0100

    Idea for handling employee, user and admin profile in ProfileController see comments

--- src/main/java/fon/bg/ac/rs/retailApp/controllers/ProfileController.java
+++ src/main/java/fon/bg/ac/rs/retailApp/controllers/ProfileController.java
@@ -2,6 +2,7 @@ package fon.bg.ac.rs.retailApp.controllers;
 
 import fon.bg.ac.rs.retailApp.models.Employee;
 import fon.bg.ac.rs.retailApp.models.User;
+import fon.bg.ac.rs.retailApp.security.models.Role;
 import fon.bg.ac.rs.retailApp.servicesImpl.EmployeeServiceImpl;
 import fon.bg.ac.rs.retailApp.servicesImpl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,9 +33,17 @@ public class ProfileController {
 
                 if(empProfile == null){
                     User user = userServiceImpl.findByUsername(username);
+//                    if (user.getRoles().isEmpty() || user.getRoles().equals(new Role(4, "USER","Ima pristup samo pocetnoj stranici i kategorijama odece (gornji delovi i donji delovi)")) ) {
+//                        model.addAttribute("myProfile", user);
+//                        return "profileUser";
+//
+//                        {
+//                    kod usera admin mozda nije stigao da mu dodeli ulogu, pa je onda set uloga prazan, ili ako nije prazan i ima ulogu user onda vracas user a
+//                    } slicnu proveru napisati ako je uloga VICE_ADMIN i roles nisu prazne, u svakom ostalom slucaju je admin
                     model.addAttribute("myProfile", user);
                     return "profileUser";
                 }
+
 
             model.addAttribute("myProfile", empProfile);
             System.out.println(empProfile);
